5bc8b23e0e63f32b3b8431fd3675e84496b43981
KishoreVB70 <kishore07kzr@gmail.com>
2022-10-20 10:25:51 +0530

    made improvements

--- contracts/AutoBan.sol
+++ contracts/AutoBan.sol
@@ -1,5 +1,7 @@
 // SPDX-License-Identifier: MIT
 
+import "@openzeppelin/contracts/utils/Counters.sol";
+
 pragma solidity >=0.7.0 <0.9.0;
 
 interface IERC20Token {
@@ -28,10 +30,11 @@ interface IERC20Token {
 }
 
 contract AutoBan {
-
-    uint256 private totalCars;
     address private cUsdTokenAddress =
         0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC1;
+
+    using Counters for Counters.Counter;
+    Counters.Counter totalCars;
 
     struct Car {
         address payable Dealer;
@@ -51,6 +54,7 @@ contract AutoBan {
     }
 
     mapping(uint256 => Car) private Cars;
+
     // keeps track of the number of cars an address has bought
     mapping(address => uint256) private NOCB;
 
@@ -65,7 +69,12 @@ contract AutoBan {
     }
 
     modifier exists(uint _index){
-        require(_index < totalCars, "Query of nonexistent car");
+        require(_index < totalCars.current(), "Query of nonexistent car");
+        _;
+    }
+
+    modifier validInput(string calldata _input){
+        require(bytes(_input).length > 0, "enter valid input");
         _;
     }
 
@@ -78,29 +87,79 @@ contract AutoBan {
         uint256 _price,
         bool _leased,
         uint leaseEndAt,
-        string memory _brand,
+        string calldata _brand,
         string calldata _image,
         string calldata _description,
-        string memory _location
-    ) public {
+        string calldata _location
+    ) public 
+      validInput(_name) validInput(_brand)
+      validInput(_image) validInput(_description)
+      validInput(_location)
+    {
         require(leaseEndAt > block.timestamp, "Invalid date for end of lease");
-        require(bytes(_name).length > 0, "Empty name");
-        require(bytes(_brand).length > 0, "Empty brand");
-        require(bytes(_image).length > 0, "Empty image");
-        require(bytes(_description).length > 0, "Empty description");
-        require(bytes(_location).length > 0, "Empty location");
-        uint256 _sold = 0;
-        Cars[totalCars] = Car(
+        Cars[totalCars.current()] = Car(
             payable(msg.sender),
             _name,
             CarData(_brand, _image, _description, _location),
             _price,
-            _sold,
+            0,
             _leased,
             leaseEndAt
         );
-        totalCars++;
+        totalCars.increment();
     }
+
+    /**
+        * @dev allow customers to buy a car that is available
+     */
+    function buyCar(uint256 _index) public payable exists(_index) canLeaseOrBuy(_index) {
+        Car storage currentCar = Cars[_index];
+        currentCar.sold++;
+        NOCB[msg.sender]++;
+        require(
+            IERC20Token(cUsdTokenAddress).transferFrom(
+                msg.sender,
+                currentCar.Dealer,
+                currentCar.price
+            ),
+            "Transfer failed."
+        );
+    }
+
+    /**
+        * @dev allow car dealers to put a car back up for sale/leasing when the leasing agreement has ended
+     */
+    function PutCarUpForLease(uint256 _index) public exists(_index) {
+        require(
+            msg.sender == Cars[_index].Dealer,
+            "This is not yours to lease"
+        );
+        require(
+            Cars[_index].leaseEndAt < block.timestamp, "Lease is not over yet"
+        );
+        Cars[_index].leased = false;
+    }
+
+    /**
+        @dev allow customers to lease a car
+        @param leaseDuration the duration of the lease agreement
+     */
+    function LeaseCar(uint256 _index, uint leaseDuration) public  exists(_index) canLeaseOrBuy(_index) {
+        require(NOCB[msg.sender] >= 3, "You need to at least buy 3 cars to be eligible to lease a car");
+        Car storage currentCar = Cars[_index];
+        uint256 LeasePrice = currentCar.price / 5;
+        currentCar.leased = true;
+        currentCar.leaseEndAt = block.timestamp + leaseDuration;
+        require(
+            IERC20Token(cUsdTokenAddress).transferFrom(
+                msg.sender,
+                currentCar.Dealer,
+                LeasePrice
+            ),
+            "Transfer failed."
+        );
+    }
+
 
     function GetCar(uint256 _index)
         public
@@ -124,59 +183,7 @@ contract AutoBan {
             Cars[_index].leaseEndAt
         );
     }
-
-    /**
-        @dev allow customers to lease a car
-        @param leaseDuration the duration of the lease agreement
-     */
-    function LeaseCar(uint256 _index, uint leaseDuration) public  exists(_index) canLeaseOrBuy(_index) {
-        require(NOCB[msg.sender] >= 3, "You need to at least buy 3 cars to be eligible to lease a car");
-        Car storage currentCar = Cars[_index];
-        uint256 LeasePrice = currentCar.price / 5;
-        currentCar.leased = true;
-        currentCar.leaseEndAt = block.timestamp + leaseDuration;
-        require(
-            IERC20Token(cUsdTokenAddress).transferFrom(
-                msg.sender,
-                currentCar.Dealer,
-                LeasePrice
-            ),
-            "Transfer failed."
-        );
-    }
-
-    /**
-        * @dev allow car dealers to put a car back up for sale/leasing when the leasing agreement has ended
-     */
-    function PutCarUpForLease(uint256 _index) public exists(_index) {
-        require(
-            msg.sender == Cars[_index].Dealer,
-            "This is not yours to lease"
-        );
-        require(
-            Cars[_index].leaseEndAt < block.timestamp, "Lease is not over yet"
-        );
-        Cars[_index].leased = false;
-    }
-
-    /**
-        * @dev allow customers to buy a car that is available
-     */
-    function buyCar(uint256 _index) public payable exists(_index) canLeaseOrBuy(_index) {
-        Car storage currentCar = Cars[_index];
-        currentCar.sold++;
-        NOCB[msg.sender]++;
-        require(
-            IERC20Token(cUsdTokenAddress).transferFrom(
-                msg.sender,
-                currentCar.Dealer,
-                currentCar.price
-            ),
-            "Transfer failed."
-        );
-    }
-
     function getTotalCars() public view returns (uint256) {
-        return (totalCars);
+        return (totalCars.current());
     }
 }
